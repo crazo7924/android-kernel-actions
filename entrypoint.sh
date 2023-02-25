ca58c02c67cfde74d3a06487baa53ea6d7ac8edc
Bharat Dev Burman <crazo7924@proton.me>
2023-02-25 23:55:27 +0530

    remove usage of make all

--- entrypoint.sh
+++ entrypoint.sh
@@ -213,7 +213,7 @@ msg "Begin building kernel..."
 
 make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all)" prepare
 
-if ! make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all) all"; then
+if ! make O=out $arch_opts $make_opts $host_make_opts -j"$(nproc --all)"; then
     err "Failed building kernel and modules, probably the toolchain is not compatible with the kernel, or kernel source problem"
     exit 3
 fi
