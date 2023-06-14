af7726b5cb2bb6b1b886b3a93841a1654e40972e
Bharat Dev Burman <crazo7924@proton.me>
2023-06-14 21:34:47 +0530

    set valid fallback kernel_path

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ python_version="$5"
 
 arch="arm64"
 zipper_path="${ZIPPER_PATH:-zipper}"
-kernel_path="${KERNEL_PATH:-.}"
+kernel_path="${KERNEL_PATH:-kernel/source}"
 workdir="$GITHUB_WORKSPACE"
 if [[ -z $python_version ]]; then
    err "Invalid python version. Must be 2 or 3"
